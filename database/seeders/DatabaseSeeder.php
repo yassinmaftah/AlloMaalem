b63783dd815ce685ce11f3ebaa8be52da12f03a4
yassinmftah <yassinmaftah06@gmail.com>
2026-02-16 15:03:28 +0000

    update DatabaseSeeder with jobs, applications, and reviews

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,60 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Job;
+use App\Models\Application;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $categories = ['Plomberie', 'Électricité', 'Peinture', 'Menuiserie', 'Jardinage', 'Maçonnerie'];
+        foreach ($categories as $cat) {
+            Category::create(['name' => $cat, 'icon' => 'fa-solid fa-toolbox']);
+        }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $Admin = User::factory()->create(['name' => 'Yassine Maftah','email' => 'admin@allomaalem.com','role' => 'admin',]);
+        $client = User::factory()->create(['name' => 'Yassine Bahajou', 'email' => 'YassineBahajou@gmail.com', 'role' => 'client']);
+        $maalem = User::factory()->create(['name' => 'Oussama Kara', 'email' => 'OussamaKara@gmail.com', 'role' => 'maalem']);
+
+        User::factory(20)->create();
+
+        $clients = User::where('role', 'client')->get();
+        $cats = Category::all();
+
+        foreach($clients as $c) {
+            Job::factory(3)->create([
+                'user_id' => $c->id,
+                'category_id' => $cats->random()->id,
+            ]);
+        }
+
+        $jobs = Job::where('status', 'open')->get();
+        $maalems = User::where('role', 'maalem')->get();
+
+        foreach($jobs as $job) {
+            $applicants = $maalems->random(2);
+            foreach($applicants as $applicant)
+            {
+                Application::factory()->create(['job_id' => $job->id,'user_id' => $applicant->id,]);
+            }
+        }
+
+        $completedJobs = Job::where('status', 'completed')->get();
+
+        foreach($completedJobs as $job) {
+            $randomMaalem = $maalems->random();
+
+            Review::factory()->create([
+                'job_id' => $job->id,
+                'reviewer_id' => $job->user_id,
+                'reviewed_id' => $randomMaalem->id,
+                'rating' => fake()->numberBetween(3, 5),
+                'comment' => fake()->sentence(),
+            ]);
+        }
     }
 }
