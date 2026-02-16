0fb439b90256b3196bd593124ae4091ae7fd5978
yassinmftah <yassinmaftah06@gmail.com>
2026-02-16 14:39:55 +0000

    create ReviewFactory for seeding ratings

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'rating' => fake()->numberBetween(1, 5),
+            'comment' => fake()->sentence(10),
+            'job_id' => 1,
+            'reviewer_id' => 1,
+            'reviewed_id' => 1,
+        ];
+    }
+}
