5230f0934161b975483e14dbe611adf69fc59836
yassinmftah <yassinmaftah06@gmail.com>
2026-02-16 18:37:24 +0000

    define Application model with fillable price and relationships

---
+++ app/Models/Application.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'proposed_price',
+        'message',
+        'status',
+        'job_id',
+        'user_id',
+    ];
+
+    public function job(){return $this->belongsTo(Job::class);}
+    public function user(){return $this->belongsTo(User::class);}
+}
