a8691f88c0f5b075b65f8c009570f9df5efda078
yassinmftah <yassinmaftah06@gmail.com>
2026-02-18 11:04:03 +0000

    implement dashboard redirection logic and profile routes

--- routes/web.php
+++ routes/web.php
@@ -2,46 +2,52 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 
-
-Route::middleware('guest')->group(function ()
-{
+Route::middleware('guest')->group(function () {
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
 });
 
-Route::middleware('auth')->group(function ()
-{
+
+Route::middleware('auth')->group(function () {
+
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/complete-profile', [ProfileController::class, 'create'])->name('profile.complete');
+    Route::post('/complete-profile', [ProfileController::class, 'store'])->name('profile.complete.store');
 
-    Route::get('/dashboard', function ()
-    {
+
+    Route::get('/dashboard', function () {
+
         /** @var \App\Models\User $user */
-        $role = auth()->user()->role;
+        $user = auth()->user();
 
-        if ($role === 'client') return redirect()->route('client.dashboard');
-        if ($role === 'maalem') return redirect()->route('maalem.dashboard');
-        if ($role === 'admin') return redirect()->route('admin.dashboard');
+
+        if ($user->role === 'admin')
+            return redirect()->route('admin.dashboard');
+
+        // this route check if the user maalem and client complate entre all info or not .
+        if (!$user->info_done)
+            return redirect()->route('profile.complete');
+
+        if ($user->role === 'client') return redirect()->route('client.dashboard');
+        if ($user->role === 'maalem') return redirect()->route('maalem.dashboard');
 
         return abort(403);
+
     })->name('dashboard');
 });
 
-Route::middleware(['auth', 'role:client'])->group(function ()
-{
-    Route::get('/client/dashboard',
-    function ()
-    {
+Route::middleware(['auth', 'role:client'])->group(function () {
+    Route::get('/client/dashboard', function () {
         return view('client.dashboard');
     })->name('client.dashboard');
 });
 
-Route::middleware(['auth', 'role:maalem'])->group(function ()
-{
-    Route::get('/maalem/dashboard', function ()
-    {
+Route::middleware(['auth', 'role:maalem'])->group(function () {
+    Route::get('/maalem/dashboard', function () {
         return view('maalem.dashboard');
     })->name('maalem.dashboard');
 });
