cf00e92017e48cebc661a7c20cd7a45d65ad2c64
yassinmftah <yassinmaftah06@gmail.com>
2026-04-19 19:13:45 +0100

    add model of payment to save data of all payment in our app

---
+++ app/Models/Payment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    protected $fillable = ['user_id', 'name', 'email', 'price'];
+
+    public function user(){return $this->belongsTo(User::class);}
+}
